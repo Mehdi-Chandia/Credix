b6627f675255ebb91fab8d364b5fead117cae8e0
Mehdi-Chandia <balochmehdi1101@gmail.com>
2026-04-21 21:05:24 +0500

    fix cors production

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(cors({
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 }));
-app.options("*", cors());
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
