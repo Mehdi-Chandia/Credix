f7308eeba5dba2bdd2321ce02c0582c691e541ee
Mehdi-Chandia <balochmehdi1101@gmail.com>
2026-04-22 10:25:16 +0500

    fix cors production

--- backend/server.js
+++ backend/server.js
@@ -10,13 +10,9 @@ dotenv.config();
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.use(cors({
-    origin: [
-        "http://localhost:5173",
-        "https://credix-sigma.vercel.app"
-    ],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+app.use(require("cors")({
+    origin: "https://credix-sigma.vercel.app",
+    credentials: true
 }));
 
 
